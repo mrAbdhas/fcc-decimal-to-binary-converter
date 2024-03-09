7635692555b19c41f384d46ab91e2594b00a36b3
mrAbdhas <143132916+mrAbdhas@users.noreply.github.com>
2024-03-09 20:14:17 +0100

    Add function, checks user input, log to console
    step2-3

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+
+//function checks user input value.
+const checkUserInput = () => {
+    console.log(numberInput.value);
+}
