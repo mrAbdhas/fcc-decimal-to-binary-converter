4833a06cc584271b86b129ced71eb7d882cda931
mrAbdhas <143132916+mrAbdhas@users.noreply.github.com>
2024-03-11 15:07:53 +0100

    Add keydown event listeenr for input element
    step5-8

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ const checkUserInput = () => {
 
 //Click event listener for convert btn, logs value of number input 
 convertBtn.addEventListener("click", checkUserInput);
+
+//keydown event listener for input element, when Enter/return key is pressed.
+numberInput.addEventListener("keydown", (e) => { //pass e as parameter to call back function. 
+    //event object is created when event listener is triggede by an event.  
+    // key property, holds the string value of the key pressed.
+    if(e.key == "Enter") { 
+        // call check user input fucntion
+        checkUserInput();
+    }
+   
+});
+
